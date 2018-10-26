7c4a795084f24b36dff8a5edbc0573fd9cdbbf7f
Jorge <wiijlg#hotmail.com>
2018-10-26 13:06:51 +0200

    Funcionalidad 1.6

--- js/script.js
+++ js/script.js
@@ -1,14 +1,21 @@
-/*
+$(function() {
+    "use strict";
 
-Style   : MobApp Script JS
-Version : 1.0
-Author  : Surjith S M
-URI     : https://surjithctly.in/
-
-Copyright © All rights Reserved 
-
-*/
-
+    /*-----------------------------------
+     * FIXED  MENU - HEADER
+     *-----------------------------------*/
+    function menuscroll() {
+        var $navmenu = $('.nav-menu');
+        if ($(window).scrollTop() > 50) {
+            $navmenu.addClass('is-scrolling');
+        } else {
+            $navmenu.removeClass("is-scrolling");
+        }
+    }
+    menuscroll();
+    $(window).on('scroll', function() {
+        menuscroll();
+    });
     /*-----------------------------------
      * NAVBAR CLOSE ON CLICK
      *-----------------------------------*/
